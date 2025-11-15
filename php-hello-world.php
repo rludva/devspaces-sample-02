315ac2b78642457900379269850ab0e03e9a9249
Radomir Ludva <noreplay@radmi.cz>
2025-11-15 19:55:43 +0000

    Correct the HTML page to test commit from devspaces..

--- php-hello-world.php
+++ php-hello-world.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Simple PHP Application</title>
-    <style>
-        body { font-family: sans-serif; text-align: center; padding-top: 50px; }
-    </style>
+  <meta charset="UTF-8">
+  <title>Simple PHP Application</title>
+  <style>
+    body { font-family: sans-serif; text-align: center; padding-top: 50px; }
+  </style>
 </head>
 <body>
+   <h1>Lorem Hello World</h1>
 
-    <?php
-    // This PHP code executes on the server-side
-    echo "<h1>Hello World! Welcome to PHP.</h1>";
-    ?>
+  <?php
+  // This PHP code executes on the server-side
+  echo "<h1>Hello World! Welcome to PHP.</h1>";
+  ?>
 
 </body>
 </html>
